3d3089bcc40b9e5df6caea1b2b74bbb312623cd9
Raghu Tirumalasetty <Raghu@MBP2020>
2020-12-03 23:14:11 -0500

    Added Public subnets

--- Proj-01/main.tf
+++ Proj-01/main.tf
@@ -1,6 +1,11 @@
 module "vpc" {
-  source   = "../modules/vpc"
-  vpc-cidr = "192.168.1.0/24"
-  vpc-name = "vpc1"
+  source       = "../modules/vpc"
+  vpc-cidr     = "192.168.1.0/24"
+  vpc-name     = "vpc1"
+  az           = ["us-east-1a", "us-east-1b"]
+  sn-cidr      = ["192.168.1.0/27", "192.168.1.32/27"]
+  sn-intent    = "Public"
+  ip-on-launch = true
 
 }
+
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,3 +7,24 @@ resource "aws_vpc" "main" {
     Name = var.vpc-name
   }
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc-name}-gw"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  count = length(var.az)
+  cidr_block = var.sn-cidr[count.index]
+  map_public_ip_on_launch = var.ip-on-launch
+  availability_zone = var.az[count.index]
+
+  tags = {
+    Name = "${var.vpc-name}-${var.sn-intent}"
+    Intent = var.sn-intent
+  }
+}
